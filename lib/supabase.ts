d3e9f8c2a8761a8db0e039186b0f38f2de71dd28
Ali <ali.lee.0923@gmail.com>
2024-07-17 11:54:53 -0400

    supabase

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl =
-  process.env.SUPABASE_URL || "https://usulmxneezppkohegsak.supabase.co";
+  process.env.SUPABASE_URL || "https://rybjhdbecuegrjvpnatc.supabase.co";
 const supabaseKey =
   process.env.SUPABASE_KEY ||
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVzdWxteG5lZXpwcGtvaGVnc2FrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjA2MDk1MDcsImV4cCI6MjAzNjE4NTUwN30.2LM1mAEey0paCX3YJpSqwljc94nCx-7FZ0B7cYNrNJ8";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5YmpoZGJlY3VlZ3JqdnBuYXRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEyMzE0OTUsImV4cCI6MjAzNjgwNzQ5NX0.XfAECiyA_TpkVSAqG833uhwaMSpe26__ISfscYHCsuQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
